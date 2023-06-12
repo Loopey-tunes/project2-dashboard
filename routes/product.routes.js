ac830a6d15cf99b54c4e84f76891c30391fba169
linhvnde <linh.nh.tyty@gmail.com>
2023-06-12 14:32:15 +0200

    adde error handler: create&list routes

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -10,14 +10,25 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 // SHOW PRODUCTS
 // // GET /products/list
 router.get("/list", isLoggedIn, async (req, res, next) => {
-  const listOfProducts = await Product.find();
-  res.render("product/products", { listOfProducts });
+  try {
+    const listOfProducts = await Product.find();
+    console.log("PRODUCTS LIST", listOfProducts);
+    res.render("product/products", { listOfProducts });
+  } catch (e) {
+    console.log("error display list of products", e);
+    next(e);
+  }
 });
 
 // CREATE PRODUCT
 // // GET /products/create
-router.get("/create", isLoggedIn, (req, res, next) => {
-  res.render("product/product-create");
+router.get("/create", isLoggedIn, async (req, res, next) => {
+  try {
+    res.render("product/product-create");
+  } catch (e) {
+    console.log("error display form to create product(s)", e);
+    next(e);
+  }
 });
 // // POST /products/create
 router.post("/create", isLoggedIn, async (req, res, next) => {
@@ -40,8 +51,14 @@ router.post("/create", isLoggedIn, async (req, res, next) => {
     });
     res.redirect("/products/list");
   } catch (e) {
-    console.log("error to post when creating new product", e);
+    console.log("error to post request when creating new product", e);
     next(e);
   }
+});
+
+// CREATE PRODUCT
+// // GET /products/create
+router.get("/create", isLoggedIn, (req, res, next) => {
+  res.render("product/product-create");
 });
 module.exports = router;
