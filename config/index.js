dd01aa74b93030a60234359376e73e7e9c89b86e
Filipe Ferreira <filipeferreira94@gmail.com>
2023-06-12 15:26:39 +0100

    Nav bar and homepage now reflect the login state

--- config/index.js
+++ config/index.js
@@ -1,65 +1,70 @@
 // We reuse this import in order to have access to the `body` property in requests
-const express = require("express");
+const express = require('express');
 
 // ℹ️ Responsible for the messages you see in the terminal as requests are coming in
 // https://www.npmjs.com/package/morgan
-const logger = require("morgan");
+const logger = require('morgan');
 
 // ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
 // https://www.npmjs.com/package/cookie-parser
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 
 // ℹ️ Serves a custom favicon on each request
 // https://www.npmjs.com/package/serve-favicon
-const favicon = require("serve-favicon");
+const favicon = require('serve-favicon');
 
 // ℹ️ global package used to `normalize` paths amongst different operating systems
 // https://www.npmjs.com/package/path
-const path = require("path");
+const path = require('path');
 
 // ℹ️ Session middleware for authentication
 // https://www.npmjs.com/package/express-session
-const session = require("express-session");
+const session = require('express-session');
 
 // ℹ️ MongoStore in order to save the user session in the database
 // https://www.npmjs.com/package/connect-mongo
-const MongoStore = require("connect-mongo");
+const MongoStore = require('connect-mongo');
 
 // Connects the mongo uri to maintain the same naming structure
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/project2-dashboard";
+	process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/project2-dashboard';
 
 // Middleware configuration
 module.exports = (app) => {
-  // In development environment the app logs
-  app.use(logger("dev"));
+	// In development environment the app logs
+	app.use(logger('dev'));
 
-  // To have access to `body` property in the request
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
+	// To have access to `body` property in the request
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: false }));
+	app.use(cookieParser());
 
-  // Normalizes the path to the views folder
-  app.set("views", path.join(__dirname, "..", "views"));
-  // Sets the view engine to handlebars
-  app.set("view engine", "hbs");
-  // AHandles access to the public folder
-  app.use(express.static(path.join(__dirname, "..", "public")));
+	// Normalizes the path to the views folder
+	app.set('views', path.join(__dirname, '..', 'views'));
+	// Sets the view engine to handlebars
+	app.set('view engine', 'hbs');
+	// AHandles access to the public folder
+	app.use(express.static(path.join(__dirname, '..', 'public')));
 
-  // Handles access to the favicon
-  app.use(
-    favicon(path.join(__dirname, "..", "public", "images", "favicon.ico"))
-  );
+	// Handles access to the favicon
+	app.use(
+		favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico'))
+	);
 
-  // ℹ️ Middleware that adds a "req.session" information and later to check that you are who you say you are 😅
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET || "super hyper secret key",
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({
-        mongoUrl: MONGO_URI,
-      }),
-    })
-  );
+	// ℹ️ Middleware that adds a "req.session" information and later to check that you are who you say you are 😅
+	app.use(
+		session({
+			secret: process.env.SESSION_SECRET || 'super hyper secret key',
+			resave: false,
+			saveUninitialized: false,
+			store: MongoStore.create({
+				mongoUrl: MONGO_URI,
+			}),
+		})
+	);
+
+	app.use((req, res, next) => {
+		res.locals.currentUser = req.session.currentUser || null;
+		next();
+	});
 };
