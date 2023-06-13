07c630ee678ac24b8e884cfeff332aa92be70a4d
linhvnde <linh.nh.tyty@gmail.com>
2023-06-13 13:00:18 +0200

    validations for product create and changed the style of error message

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,46 +1,55 @@
-const fs = require('fs');
-const csv = require('csv-parser');
-const express = require('express');
-const upload = require('../middleware/upload');
+const fs = require("fs");
+const mongoose = require("mongoose")
+const csv = require("csv-parser");
+const express = require("express");
+const upload = require("../middleware/upload");
 const router = express.Router();
 
-const Product = require('../models/Product.model');
+const Product = require("../models/Product.model");
 
-const isLoggedIn = require('../middleware/isLoggedIn');
+const isLoggedIn = require("../middleware/isLoggedIn");
 
 ////------------- ROUTES------------------
 
 // SHOW PRODUCTS
 // // GET /products/list
-router.get('/list', isLoggedIn, async (req, res, next) => {
-	try {
-		const listOfProducts = await Product.find();
-		res.render('product/product-list', { listOfProducts });
-	} catch (e) {
-		console.log('error display list of products', e);
-		next(e);
-	}
+router.get("/list", isLoggedIn, async (req, res, next) => {
+  try {
+    const listOfProducts = await Product.find();
+    res.render("product/product-list", { listOfProducts });
+  } catch (e) {
+    console.log("error display list of products", e);
+    next(e);
+  }
 });
 
 // CREATE PRODUCT
 // // GET /products/create
-router.get('/create', isLoggedIn, async (req, res, next) => {
-	try {
-		res.render('product/product-create');
-	} catch (e) {
-		console.log('error display form to create product(s)', e);
-		next(e);
-	}
+router.get("/create", isLoggedIn, async (req, res, next) => {
+  try {
+    res.render("product/product-create");
+  } catch (e) {
+    console.log("error display form to create product(s)", e);
+    next(e);
+  }
 });
 // // POST /products/create
-router.post('/create', isLoggedIn, async (req, res, next) => {
-	const { name, ref, stock, image, category, keywords, manufacturer } =
-		req.body;
-	const location = {
-		row: req.body.row,
-		lane: req.body.lane,
-		shelf: req.body.shelf,
+router.post("/create", isLoggedIn, async (req, res, next) => {
+  const { name, ref, stock, image, category, keywords, manufacturer } =
+    req.body;
+  const location = {
+    row: req.body.row,
+    lane: req.body.lane,
+    shelf: req.body.shelf,
 	};
+	
+	//Validate if all required fields are provided
+  if (!name || !ref || !stock || !manufacturer) {
+    res.render("product/product-create", {
+      errorMessage: "All fields (*) are mandatory.",
+    });
+    return;
+  }
 
 	try {
 		await Product.create({
@@ -53,112 +62,120 @@ router.post('/create', isLoggedIn, async (req, res, next) => {
 			keywords,
 			manufacturer,
 		});
-		res.redirect('/products/list');
+		res.redirect("/products/list");
 	} catch (e) {
-		console.log('error to post request when creating new product', e);
-		next(e);
+		console.log("error to post request when creating new product", e);
+		if (e instanceof mongoose.Error.ValidationError) {
+			console.log("this is a mongoose validator error")
+			res.status(400).render('product/product-create', { errorMessage: e.message })
+		} else if (e.code === 11000) {
+			res.status(400).render('product/product-create', { errorMessage: 'Product Reference needs to be unique.' });
+		} else {
+			console.log("this is NOT a mongoose validator error");
+			next(e);
+		}
 	}
 });
 
 // EDIT PRODUCT
 // // GET /products/:productId/edit
-router.get('/:productId/edit', isLoggedIn, async (req, res, next) => {
-	try {
-		const productId = req.params.productId;
-		const productToEdit = await Product.findById(productId);
-		res.render('product/product-edit', { productToEdit });
-	} catch (e) {
-		console.log('error to show update product form', e);
-		next(e);
-	}
+router.get("/:productId/edit", isLoggedIn, async (req, res, next) => {
+  try {
+    const productId = req.params.productId;
+    const productToEdit = await Product.findById(productId);
+    res.render("product/product-edit", { productToEdit });
+  } catch (e) {
+    console.log("error to show update product form", e);
+    next(e);
+  }
 });
 
 // IMPORT PRODUCTS
 // // POST /products/create
-router.post('/import', upload.single('product-list'), async (req, res) => {
-	// console.log(
-	// "REQUEST DETAILS OF THE UPLOAD....req.file",
-	// req.file,
-	// "REQUEST DETAILS OF THE UPLOAD....req.body",
-	// req.body
-	// );
-	const results = [];
+router.post("/import", upload.single("product-list"), async (req, res) => {
+  // console.log(
+  // "REQUEST DETAILS OF THE UPLOAD....req.file",
+  // req.file,
+  // "REQUEST DETAILS OF THE UPLOAD....req.body",
+  // req.body
+  // );
+  const results = [];
 
-	fs.createReadStream(req.file.path)
-		.pipe(csv())
-		.on('data', (data) => results.push(data))
-		.on('end', async () => {
-			try {
-				const products = await Product.create(results);
-				console.log('THIS IS LIST OF PRODUCTS IMPORTED--------', products);
-				res.render('product/product-import', { products });
-			} catch (
-				e
-				// handle error
-			) {
-				console.error(e);
-				res.status(500).send('Error occurred when create products by imported');
-			}
-		});
+  fs.createReadStream(req.file.path)
+    .pipe(csv())
+    .on("data", (data) => results.push(data))
+    .on("end", async () => {
+      try {
+        const products = await Product.create(results);
+        console.log("THIS IS LIST OF PRODUCTS IMPORTED--------", products);
+        res.render("product/product-import", { products });
+      } catch (
+        e
+        // handle error
+      ) {
+        console.error(e);
+        res.status(500).send("Error occurred when create products by imported");
+      }
+    });
 });
 
 // // POST /products/:productId/edit
-router.post('/:productId/edit', isLoggedIn, async (req, res, next) => {
-	const productId = req.params.productId;
-	const { name, ref, stock, image, category, keywords, manufacturer } =
-		req.body;
-	const location = {
-		row: req.body.row,
-		lane: req.body.lane,
-		shelf: req.body.shelf,
-	};
-	console.log(
-		'THIS IS THE ID',
-		productId,
-		'THIS IS UPDATED PRODUCT',
-		name,
-		ref,
-		stock,
-		image,
-		category,
-		keywords,
-		manufacturer,
-		location
-	);
-	try {
-		const updatedProduct = await Product.findByIdAndUpdate(
-			productId,
-			{
-				name,
-				ref,
-				stock,
-				image,
-				location,
-				category,
-				keywords,
-				manufacturer,
-			},
-			{ new: true }
-		);
-		console.log('THIS IS UPDATED PRODUCT ', updatedProduct);
-		res.redirect('/products/list');
-	} catch (e) {
-		console.log('error to put update product', e);
-		next(e);
-	}
+router.post("/:productId/edit", isLoggedIn, async (req, res, next) => {
+  const productId = req.params.productId;
+  const { name, ref, stock, image, category, keywords, manufacturer } =
+    req.body;
+  const location = {
+    row: req.body.row,
+    lane: req.body.lane,
+    shelf: req.body.shelf,
+  };
+  console.log(
+    "THIS IS THE ID",
+    productId,
+    "THIS IS UPDATED PRODUCT",
+    name,
+    ref,
+    stock,
+    image,
+    category,
+    keywords,
+    manufacturer,
+    location
+  );
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      {
+        name,
+        ref,
+        stock,
+        image,
+        location,
+        category,
+        keywords,
+        manufacturer,
+      },
+      { new: true }
+    );
+    console.log("THIS IS UPDATED PRODUCT ", updatedProduct);
+    res.redirect("/products/list");
+  } catch (e) {
+    console.log("error to put update product", e);
+    next(e);
+  }
 });
 
 // DELETE PRODUCT
 // // POST /products/:productId/edit
-router.get('/:productId/delete', isLoggedIn, async (req, res, next) => {
-	try {
-		const productId = req.params.productId;
-		await Product.findByIdAndDelete(productId);
-		res.redirect('/products/list');
-	} catch (e) {
-		console.log('error to delete a product', e);
-		next(e);
-	}
+router.get("/:productId/delete", isLoggedIn, async (req, res, next) => {
+  try {
+    const productId = req.params.productId;
+    await Product.findByIdAndDelete(productId);
+    res.redirect("/products/list");
+  } catch (e) {
+    console.log("error to delete a product", e);
+    next(e);
+  }
 });
 
 // SHOW PRODUCT DETAILS
