73fd534b80cda7f5070611864d3bfff1ea799c6d
linhvnde <linh.nh.tyty@gmail.com>
2023-06-15 00:05:21 +0200

    sort functionality in product-list

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ const hbs = require('hbs');
 hbs.registerHelper('increment', function (value) {
 	return parseInt(value) + 1;
 });
+hbs.registerHelper("sortingButtons", function (column, options) {
+  const ascButton = `<button onclick="window.location.href='/products/sort?col=${column}&ord=asc'" class="btn btn-light btn-sm" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .2rem; --bs-btn-font-size: .7rem;" >↑</button>`;
+  const descButton = `<button onclick="window.location.href='/products/sort?col=${column}&ord=desc'" class="btn btn-light btn-sm" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .2rem; --bs-btn-font-size: .7rem;" >↓</button>`;
+  return new hbs.SafeString(ascButton + descButton);
+});
+
+
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -12,6 +12,7 @@ const isLoggedIn = require('../middleware/isLoggedIn');
 
 ////------------- ROUTES------------------
 
+
 // SHOW PRODUCTS
 // // GET /products/list
 router.get('/list', isLoggedIn, async (req, res, next) => {
@@ -22,6 +23,26 @@ router.get('/list', isLoggedIn, async (req, res, next) => {
 		console.log('error display list of products', e);
 		next(e);
 	}
+});
+
+// SORT PRODUCTS
+// // GET /products/sort
+router.get('/sort', isLoggedIn, async (req, res, next) => {
+  try {
+    const query = {};
+
+    const { col, ord } = req.query;
+    if (ord === "asc") {
+      query[col] = 1
+    } else {
+      query[col] = -1
+    }
+    const listOfProducts = await Product.find().sort(query);
+    res.render("product/product-list", {listOfProducts})
+  } catch (e) {
+    console.log('error sort list of products', e);
+    next(e);
+  }
 });
 
 // CREATE PRODUCT
@@ -46,8 +67,8 @@ router.post(
       lane: req.body.lane,
       shelf: req.body.shelf,
     };
-
-
+    
+    
     let imagePath;
     if (req.file) {
       imagePath = req.file.path;
@@ -91,21 +112,21 @@ router.post(
       }
     }
   }
-);
-
-// IMPORT PRODUCTS
-// // POST /products/create
-router.post("/import", upload.single("product-list"), async (req, res) => {
-  const results = [];
-  if (!req.file) {
-    res.render("product/product-create", {
-      errorMessage: "No file was uploaded.",
+  );
+  
+  // IMPORT PRODUCTS
+  // // POST /products/create
+  router.post("/import", upload.single("product-list"), async (req, res) => {
+    const results = [];
+    if (!req.file) {
+      res.render("product/product-create", {
+        errorMessage: "No file was uploaded.",
     });
     return;
   }
   fs.createReadStream(req.file.path)
-    .pipe(csv())
-    .on("data", (data) => {
+  .pipe(csv())
+  .on("data", (data) => {
       // Check if all required fields exist in the data row
       if (!data.name || !data.ref || !data.stock) {
         res.render("product/product-create", {
@@ -118,7 +139,7 @@ router.post("/import", upload.single("product-list"), async (req, res) => {
 			results.push(data);
 		})
 		.on('end', async () => {
-			// Check if any data is present
+      // Check if any data is present
 			if (results.length === 0) {
 				res.render('product/product-create', {
 					errorMessage: 'The uploaded CSV file is empty.',
@@ -126,40 +147,40 @@ router.post("/import", upload.single("product-list"), async (req, res) => {
 				return;
 			}
 			try {
-				const products = await Product.create(results);
+        const products = await Product.create(results);
 				res.render('product/product-import', { products });
 			} catch (
 				e
 				// handle error
 			) {
-				console.log('error when creating new product by importing from csv', e);
+        console.log('error when creating new product by importing from csv', e);
 				if (e instanceof mongoose.Error.ValidationError) {
-					console.log('this is a mongoose validator error');
+          console.log('this is a mongoose validator error');
 					res
-						.status(400)
-						.render('product/product-create', { errorMessage: e.message });
+          .status(400)
+          .render('product/product-create', { errorMessage: e.message });
 				} else if (e.code === 11000) {
-					res.status(400).render('product/product-create', {
-						errorMessage: 'Product Reference needs to be unique.',
+          res.status(400).render('product/product-create', {
+            errorMessage: 'Product Reference needs to be unique.',
 					});
 				} else {
-					console.log('this is NOT a mongoose validator error');
+          console.log('this is NOT a mongoose validator error');
 					next(e);
 				}
 			}
 		});
-});
+  });
 
-// EDIT PRODUCT
-// // GET /products/:productId/edit
-router.get('/:productId/edit', isLoggedIn, async (req, res, next) => {
-	try {
+  // EDIT PRODUCT
+  // // GET /products/:productId/edit
+  router.get('/:productId/edit', isLoggedIn, async (req, res, next) => {
+    try {
 		const productId = req.params.productId;
 		const productToEdit = await Product.findById(productId);
 		console.log('productToEdit.id', productToEdit.id);
 		res.render('product/product-edit', { productToEdit });
 	} catch (e) {
-		console.log('error to show update product form', e);
+    console.log('error to show update product form', e);
 		next(e);
 	}
 });
@@ -172,7 +193,7 @@ router.post(
     const productId = req.params.productId;
     const { name, ref, stock, image, category, keywords, manufacturer } =
       req.body;
-    const location = {
+      const location = {
       row: req.body.row,
       lane: req.body.lane,
       shelf: req.body.shelf,
@@ -215,7 +236,7 @@ router.post(
           manufacturer,
         },
         { new: true }
-      );
+        );
       res.redirect("/products/list");
     } catch (e) {
       console.log("error when editing product with the post request", e);
@@ -265,7 +286,7 @@ router.get('/:productId/delete', isLoggedIn, async (req, res, next) => {
 		await Product.findByIdAndDelete(productId);
 		res.redirect('/products/list');
 	} catch (e) {
-		console.log('error to delete a product', e);
+    console.log('error to delete a product', e);
 		next(e);
 	}
 });
@@ -273,8 +294,8 @@ router.get('/:productId/delete', isLoggedIn, async (req, res, next) => {
 // SHOW PRODUCT DETAILS
 // // GET /products/:productId
 router.get('/:productId', isLoggedIn, async (req, res, next) => {
-	try {
-		const productId = req.params.productId;
+  try {
+    const productId = req.params.productId;
 		const productToView = await Product.findById(productId);
 		res.render('product/product-details', { productToView });
 	} catch (e) {
@@ -283,32 +304,31 @@ router.get('/:productId', isLoggedIn, async (req, res, next) => {
 	}
 });
 
-module.exports = router;
 
 //Search with filters
 
 router.post('/search', isLoggedIn, async (req, res, next) => {
-	try {
-		const query = {};
-
+  try {
+    const query = {};
+    
 		//query for name/ref/category/manufacturer filter
 		const { filter, input } = req.body;
-
+    
 		if (filter && input) {
-			query[filter] = { $regex: new RegExp(input, 'i') }; //regex to make it case insensitive
+      query[filter] = { $regex: new RegExp(input, 'i') }; //regex to make it case insensitive
 		}
-
+    
 		//query for row/lane/shelf
-
+    
 		const { row, lane, shelf } = req.body;
-
+    
 		if (row || lane || shelf) {
-			const parsedRow = parseInt(row);
+      const parsedRow = parseInt(row);
 			const parsedLane = parseInt(lane);
 			const parsedShelf = parseInt(shelf);
 
 			if (!isNaN(parsedRow)) {
-				query['location.row'] = parsedRow;
+        query['location.row'] = parsedRow;
 			}
 			if (!isNaN(parsedLane)) {
 				query['location.lane'] = parsedLane;
@@ -317,42 +337,47 @@ router.post('/search', isLoggedIn, async (req, res, next) => {
 				query['location.shelf'] = parsedShelf;
 			}
 		}
-
+    
 		//query for stock value
-
+    
 		const { stockFilter, stockInput } = req.body;
-
+    
 		if (stockFilter && stockInput) {
 			const parsedStockInput = parseInt(stockInput);
 
 			if (!isNaN(parsedStockInput)) {
-				switch (stockFilter) {
-					case 'gte':
-						query.stock = { $gte: parsedStockInput };
+        switch (stockFilter) {
+          case 'gte':
+            query.stock = { $gte: parsedStockInput };
 						break;
-					case 'gt':
-						query.stock = { $gt: parsedStockInput };
+            case 'gt':
+              query.stock = { $gt: parsedStockInput };
 						break;
 					case 'lte':
-						query.stock = { $lte: parsedStockInput };
+            query.stock = { $lte: parsedStockInput };
 						break;
-					case 'lt':
-						query.stock = { $lt: parsedStockInput };
-						break;
+            case 'lt':
+              query.stock = { $lt: parsedStockInput };
+              break;
 					default:
 						break;
-				}
+          }
 			}
 		}
-
+    
 		console.log('----------------query----------------> ', query);
 
 		const listOfProducts = await Product.find(query);
-
+    
 		res.render('product/product-list', { listOfProducts });
 	} catch (error) {
-		console.log(error);
-
+    console.log(error);
+    
 		next(error);
 	}
 });
+
+
+
+// EXPORT ROUTERS
+module.exports = router;
