74cfbfb42974cfd8dcf68ab5a7558cf1bfd00338
linhvnde <linh.nh.tyty@gmail.com>
2023-06-14 14:01:41 +0200

    back to mongoose

--- db/index.js
+++ db/index.js
@@ -5,48 +5,17 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-// const MONGO_URI =
-//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/project2-dashboard";
-
-// mongoose
-//   .connect(MONGO_URI)
-//   .then((x) => {
-//     const databaseName = x.connections[0].name;
-//     console.log(`Connected to Mongo! Database name: "${databaseName}"`);
-//   })
-//   .catch((err) => {
-//     console.error("Error connecting to mongo: ", err);
-//   });
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
+const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/project2-dashboard";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    const databaseName = x.connections[0].name;
+    console.log(`Connected to Mongo! Database name: "${databaseName}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } catch (e) {
-    console.log("theres an error to connect mongodb", e);
-  }
-  // finally {
-  //   // Ensures that the client will close when you finish/error
-  //   await client.close();
-  //   console.log("Connection closed");
-  // }
-}
-run().catch(console.dir);
+
