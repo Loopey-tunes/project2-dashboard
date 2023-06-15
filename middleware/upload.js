e34db546a37e2419eed9a968fdfd8b3d9f60ac47
linhvnde <linh.nh.tyty@gmail.com>
2023-06-15 11:29:51 +0200

    change upload dir to public

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./upload");
+    cb(null, "./public/upload");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "--" + file.originalname);
